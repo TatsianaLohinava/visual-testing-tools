505dd22956fff3421d9d24ec5aee8c2e23b60115
TatsianaLohinava <tlohinava@solvd.com>
2024-06-13 17:53:46 +0300

    add chromatic

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,12 @@
 const { defineConfig } = require("cypress");
+const { installPlugin } = require("@chromatic-com/cypress");
 
 module.exports = defineConfig({
   viewportWidth: 1000,
   viewportHeight: 660,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      installPlugin(on, config);
     },
   },
 
