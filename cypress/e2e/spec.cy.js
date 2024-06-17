169bcb934a006e292219c642145ea9cd0e2bf2a2
TatsianaLohinava <tlohinava@solvd.com>
2024-06-17 12:50:37 +0300

    remove driver

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/');
-    cy.percySnapshot(driver, 'dashboard page', {scope: 'header'});
+    cy.percySnapshot('dashboard page', {scope: 'header'});
   })
 })
