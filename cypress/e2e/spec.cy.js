3828a43d1a1bec9943ed71c14344e1e7116e51c4
TatsianaLohinava <tlohinava@solvd.com>
2024-06-17 13:50:39 +0300

    add snapshot settings

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/');
-    cy.percySnapshot('dashboard page', {scope: 'header'});
+    cy.percySnapshot('dashboard page');
+    cy.percySnapshot('dashboard header', {scope: 'header'}, {testCase: 'header'});
   })
 })
