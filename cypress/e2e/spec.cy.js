f500f31ab4da5e150283a9d50213d6678baa5921
TatsianaLohinava <tlohinava@solvd.com>
2024-06-17 13:54:53 +0300

    change args

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,6 +2,6 @@ describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/');
     cy.percySnapshot('dashboard page');
-    cy.percySnapshot('dashboard header', {scope: 'header'}, {testCase: 'header'});
+    cy.percySnapshot('dashboard header', {scope: 'header', testCase: 'header'});
   })
 })
