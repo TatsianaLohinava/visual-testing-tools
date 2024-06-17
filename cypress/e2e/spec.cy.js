cf863234e24388e72427a5b741e5e0d071fa2974
TatsianaLohinava <tlohinava@solvd.com>
2024-06-17 15:07:33 +0300

    add layout diff setting

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,7 +1,7 @@
 describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/');
-    cy.percySnapshot('dashboard page');
-    cy.percySnapshot('dashboard header', {scope: 'header', testCase: 'header'});
+    cy.percySnapshot('dashboard page', { enableLayout: true });
+    cy.percySnapshot('dashboard header', { scope: 'header', testCase: 'header' });
   })
 })
