0b915dba18d72542df7ef9673835a12104c641a8
TatsianaLohinava <tlohinava@solvd.com>
2024-06-18 11:48:38 +0300

    introduce changes

--- src/Dashboard.tsx
+++ src/Dashboard.tsx
@@ -123,7 +123,7 @@ export default function Dashboard() {
               noWrap
               sx={{ flexGrow: 1 }}
             >
-              Dashboard
+              my Dashboard
             </Typography>
             <IconButton color="inherit">
               <Badge badgeContent={4} color="secondary">
