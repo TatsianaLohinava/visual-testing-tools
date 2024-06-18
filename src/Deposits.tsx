491c9d61af9790bfed2891b0138dc4e5b1f4b4e6
TatsianaLohinava <tlohinava@solvd.com>
2024-06-18 17:12:19 +0300

    add ignore attribute

--- src/Deposits.tsx
+++ src/Deposits.tsx
@@ -11,8 +11,8 @@ export default function Deposits() {
   return (
     <React.Fragment>
       <Title>Recent Deposits</Title>
-      <Typography component="p" variant="h4">
-        $3,024.00
+      <Typography component="p" variant="h4" data-happo-hide="">
+        $3,000.00
       </Typography>
       <Typography color="text.secondary" sx={{ flex: 1 }}>
         on 15 March, 2019
