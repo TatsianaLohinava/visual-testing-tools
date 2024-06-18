64ae86951caf5622394f836b85e7ee83fcfbb22c
TatsianaLohinava <tlohinava@solvd.com>
2024-06-18 12:11:46 +0300

    add heading screenshot

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,6 @@ describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/')
     cy.get('header').happoScreenshot()
+    cy.get('h1').happoScreenshot()
   })
 })
