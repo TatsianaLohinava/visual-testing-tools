3cde44c52259e427a78e4c9bfbc88444ffa1b3a3
TatsianaLohinava <tlohinava@solvd.com>
2024-06-18 17:30:48 +0300

    add changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@ describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/')
     cy.get('header').happoScreenshot()
-    // cy.get('h1').happoScreenshot()
+    cy.get('h1').happoScreenshot()
     cy.get('main').happoScreenshot();
   })
 })
