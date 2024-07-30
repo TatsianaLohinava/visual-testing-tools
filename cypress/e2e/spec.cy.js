f360b29db41a8ffa8d495eeca5bb94b5ac566335
TatsianaLohinava <tlohinava@solvd.com>
2024-07-30 13:54:13 +0300

    add changhes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,36 @@
-describe('template spec', () => {
+describe('Dashboard', () => {
+  before(() => {
+    
+  })
+  beforeEach(() => {
+    cy.eyesOpen({
+      appName: 'Dashboard',
+      testName: 'visits page',
+      browser: [
+        {width: 800, height: 600, name: 'firefox'},
+        {width: 1024, height: 768, name: 'chrome'},
+        {width: 1024, height: 768, name: 'ie11'},
+        {
+          deviceName: 'iPhone X',
+          screenOrientation: 'portrait',
+          name: 'chrome' // optional, just to make it explicit this is browser emulation and not a real device. Only chrome is supported for device emulation.
+        }
+      ]
+    })
+  })
+
+
+  afterEach(() => {
+    cy.eyesClose()
+  })
+
   it('visits page', () => {
     cy.visit('http://localhost:5173/')
+    //assertion to check if the page is rendered
+    cy.eyesCheckWindow({
+      tag: "Dashboard page",
+      // matchLevel: 'Ignore Colors'
+    });
   })
+
 })
