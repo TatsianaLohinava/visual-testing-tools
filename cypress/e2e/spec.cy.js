9f613588ff9817a4cf49796d544445425fbfea53
TatsianaLohinava <tlohinava@solvd.com>
2024-07-31 16:52:27 +0300

    enable layout testing

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,9 @@
 describe('template spec', () => {
-  it('visits page', () => {
+  it('toggles drawer', () => {
     cy.visit('http://localhost:5173/')
     cy.get('[aria-label="close drawer"]').click()
-    cy.percySnapshot('closed drawer')
+    cy.percySnapshot('closed drawer', { enableLayout: true })
     cy.get('[aria-label="open drawer"]').click()
-    cy.percySnapshot('open drawer')
+    cy.percySnapshot('open drawer', { enableLayout: true })
   })
 })
