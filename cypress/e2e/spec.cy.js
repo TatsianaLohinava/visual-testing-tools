31b38e47017f1b4b47bab783ffb44b3b4cbbab1b
TatsianaLohinava <tlohinava@solvd.com>
2024-07-31 17:01:17 +0300

    adjust test scopes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,9 @@ describe('template spec', () => {
   it('toggles drawer', () => {
     cy.visit('http://localhost:5173/')
     cy.get('[aria-label="close drawer"]').click()
-    cy.percySnapshot('closed drawer', { enableLayout: true })
+    cy.percySnapshot('closed drawer ')
+    cy.percySnapshot('closed drawer layout', { enableLayout: true })
+    cy.percySnapshot('closed drawer header', { scope: 'header' })
     cy.get('[aria-label="open drawer"]').click()
     cy.percySnapshot('open drawer', { enableLayout: true })
   })
