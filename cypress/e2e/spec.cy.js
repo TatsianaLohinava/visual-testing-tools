577ea40939b75356d4d0beaf03bbdcbd1c704b6e
TatsianaLohinava <tlohinava@solvd.com>
2024-07-31 16:41:44 +0300

    adjust test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,5 +2,8 @@ describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/')
     cy.get('[aria-label="close drawer"]').click()
+    cy.percySnapshot('closed drawer')
+    cy.get('[aria-label="open drawer"]').click()
+    cy.percySnapshot('open drawer')
   })
 })
