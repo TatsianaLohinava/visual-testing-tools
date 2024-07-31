df4e098856fc0fdd71c19b0009fb3b7f6c71f12b
TatsianaLohinava <tlohinava@solvd.com>
2024-07-31 14:58:14 +0300

    add snapshot

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 describe('template spec', () => {
   it('visits page', () => {
     cy.visit('http://localhost:5173/')
+    cy.takeSnapshot()
     cy.get('[aria-label="close drawer"]').click()
   })
 })
