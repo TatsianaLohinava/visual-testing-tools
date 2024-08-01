7e1d67dfa67b6d903758b2503ca0763ef5429460
TatsianaLohinava <tlohinava@solvd.com>
2024-08-01 11:48:36 +0300

    add changes

--- src/theme.tsx
+++ src/theme.tsx
@@ -5,7 +5,7 @@ import { red, green } from "@mui/material/colors";
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: green[900],
     },
     secondary: {
       main: "#19857b",
