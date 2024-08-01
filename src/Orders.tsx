1dad86b8ef570e50dd5e24e1fcf95da53670104a
TatsianaLohinava <tlohinava@solvd.com>
2024-08-01 16:36:26 +0300

    change heading

--- src/Orders.tsx
+++ src/Orders.tsx
@@ -62,7 +62,7 @@ function preventDefault(event: React.MouseEvent) {
 export default function Orders() {
   return (
     <React.Fragment>
-      <Title>Latest Orders</Title>
+      <Title>Recent Orders</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
